b6ac8492aac93d8369f981abb614d8a73294297a
Titus Soporan <tsopor@gmail.com>
2011-05-08 14:11:38 -0400

    Admin display update.

--- pastebin/admin.py
+++ pastebin/admin.py
@@ -3,11 +3,14 @@ from paest.pastebin.models import Snippet
 import datetime
 
 class SnippetAdmin(admin.ModelAdmin):
-    list_display = ('id', 'url', 'created', 'has_expired')
+    list_display = ('id', 'code_head', 'url', 'created', 'has_expired')
 
     def has_expired(self, obj):
         if obj.expires_date:
             return datetime.datetime.now() > obj.expires_date
         else: return False
+    
+    def code_head(self, obj):
+        return obj.code[:40] + ' ...'
 
 admin.site.register(Snippet, SnippetAdmin)
